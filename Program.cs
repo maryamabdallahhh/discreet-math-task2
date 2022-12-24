79c5cb44aab5adf1dc22b607cea3f1a6ad516b6f
maryamabdallahhh <121059049+maryamabdallahhh@users.noreply.github.com>
2022-12-24 13:34:07 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Security;
+
+Console.Write("Enter the  first number to  : ");
+int num1 =int.Parse(Console.ReadLine());
+Console.Write("Enter the the second number : ");
+int num2 =int.Parse(Console.ReadLine());
+Console.Write("the perfect numbers betwen " + num1 + " and " + num2 + " is : ");
+ for(int i = num1; i < num2; i++)
+{
+    int n = i, sum = 0, factor = 1;
+    while (factor < n)
+    {
+        if (n % factor == 0)
+            sum = sum + factor;
+        factor++;
+    }
+    if (sum == i) Console.WriteLine(i + "");
+}
